bb7dffd1053e2a4a39c9834da21fbbf0c4a916fd
Omel1311 <omel1311@gmail.com>
2022-07-30 12:49:19 +0300

    яблука в школі (з перевркою РЕР-8)

--- Lesson_4/task_3.py
+++ Lesson_4/task_3.py
@@ -1,14 +1,13 @@
 password = 12345
 
-user_password = int (input('Введіть будь-ласка пароль!: '))
+user_password = int(input('Введіть будь-ласка пароль!: '))
 
 if user_password != password:
     print("Невірно! Вам відмовлено в доступі!")
-    user_password_2 = int (input('Введіть будь-ласка повторно пароль!: '))
+    user_password_2 = int(input('Введіть будь-ласка повторно пароль!: '))
     if user_password_2 == password:
         print("Пароль вірний! Ви отримали доступ!")
     else:
         print("Відмовлено в доступі повторно! Ви заблоковані!")
 else:
     print("Пароль вірний! Ви отримали доступ!")
-
