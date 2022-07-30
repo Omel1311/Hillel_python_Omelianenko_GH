a0a0ee91a9ade8e25adca1802474a7ee3b4a88e8
Omel1311 <omel1311@gmail.com>
2022-07-30 19:05:48 +0300

    2 версія 2 завдання

---
+++ Lesson_4/task_2_2version.py
@@ -0,0 +1,17 @@
+import math
+
+class_1 = int(input('Скільки учнів в першому класі? : '))
+
+class_2 = int(input('Скільки учнів в третьому класі? : '))
+
+class_3 = int(input('Скільки учнів в третьому класі? : '))
+
+desk_1 = math.ceil(class_1 / 2)
+
+desk_2 = math.ceil(class_2 / 2)
+
+desk_3 = math.ceil(class_3 / 2)
+
+desk_all = (desk_1 + desk_2 + desk_3)
+
+print('Всього потрібно закупити' + str(desk_all) + ' парт!')
