6d06edce26d9c678158892031c51ba13908c4d55
Omel1311 <omel1311@gmail.com>
2022-07-30 17:52:58 +0300

    доопрацьована з урахуванням зауважень викладача

--- Lesson_4/task_2.py
+++ Lesson_4/task_2.py
@@ -4,8 +4,24 @@ class_2 = int(input('Скільки учнів в третьому класі? :
 
 class_3 = int(input('Скільки учнів в третьому класі? : '))
 
-desk = (class_1+class_2+class_3) // 2
+desk_1 = class_1 // 2
 
-desk_all = desk + ((class_1+class_2+class_3) % 2)
+desk_11 = class_1 % 2
+
+desk_111 = desk_1 + desk_11
+
+desk_2 = class_2 // 2
+
+desk_22 = class_2 % 2
+
+desk_222 = desk_2 + desk_22
+
+desk_3 = class_3 // 2
+
+desk_33 = class_3 % 2
+
+desk_333 = desk_3 + desk_33
+
+desk_all = (desk_111 + desk_222 + desk_333)
 
 print('Всього потрібно закупити ' + str(desk_all) + ' парт!')
