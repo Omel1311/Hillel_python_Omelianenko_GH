2749a19d1dd9ec7b43b1d925111540f793cae3d6
Omel1311 <omel1311@gmail.com>
2022-08-11 17:34:00 +0300

    зсув вліво без insert2

--- Lesson_7/task_2.py
+++ Lesson_7/task_2.py
@@ -2,15 +2,19 @@ while True:
     q = input('Введіть список із цілих чисел: ')
     a = list(q)
     k = int(input('Введіть число (індекс К): '))
-    c = int(input('Введіть значення (число С): '))
-    ap = input("Введіть значення, яке буде додано в кінці списку: ")
-    print("*" * 60)
-    print(f"Було: {a}")
 
-    for i in range(len(a) - 1, k, -1):
-        a[i] = a[i - 1]
+    if k <= len(q):
+        c = int(input('Введіть значення (число С): '))
+        ap = input("Введіть значення, яке буде додано в кінці списку: ")
+        print("*" * 60)
+        print(f"Було: {a}")
 
-    a[k] = c
-    a.append(ap)
-    print(f"Cтало:{a}")
-    print("*"*60)
+        for i in range(len(a) - 1, k, -1):
+            a[i] = a[i - 1]
+
+        a[k] = c
+        a.append(ap)
+        print(f"Cтало:{a}")
+        print("*"*60)
+    else:
+        print("Завелике число! Введіть ще раз")
