09b687e2e5f84751f93a3af7d4b5e4376abccd6f
Omel1311 <omel1311@gmail.com>
2022-08-11 17:20:40 +0300

    зсув вліво без insert

---
+++ Lesson_7/task_2.py
@@ -0,0 +1,16 @@
+while True:
+    q = input('Введіть список із цілих чисел: ')
+    a = list(q)
+    k = int(input('Введіть число (індекс К): '))
+    c = int(input('Введіть значення (число С): '))
+    ap = input("Введіть значення, яке буде додано в кінці списку: ")
+    print("*" * 60)
+    print(f"Було: {a}")
+
+    for i in range(len(a) - 1, k, -1):
+        a[i] = a[i - 1]
+
+    a[k] = c
+    a.append(ap)
+    print(f"Cтало:{a}")
+    print("*"*60)
