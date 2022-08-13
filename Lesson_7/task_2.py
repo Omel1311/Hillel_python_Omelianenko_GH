f159323fa9e168367aa303df5ef9da69d3ff4e32
Omel1311 <omel1311@gmail.com>
2022-08-13 12:00:23 +0300

    55

--- Lesson_7/task_2.py
+++ Lesson_7/task_2.py
@@ -1,4 +1,4 @@
-hile True:
+while True:
     q = input('Введіть список із цілих чисел: ')
     a = list(q)
     k = int(input('Введіть число (індекс К): '))
@@ -7,12 +7,11 @@ hile True:
     if k <= len(q):
         print("*" * 60)
         print(f"Було: {a}")
-        a.append(3) # можна ввести будь-яке значеня
+        a.append(3) # можна ввести будь-яке значеня.
         print(f"Проміжне значення: {a}")
 
         for i in range(len(a) - 1, k, -1):
             a[i] = a[i - 1]
-
         a[k] = c
         print(f"Cтало:{a}")
         print("*"*60)
