a9f3db5aeb7c48bac5e6125ccb999708b56f3437
Omel1311 <omel1311@gmail.com>
2022-08-15 18:00:26 +0300

    матриця (з random)/сума

--- Lesson_8/task_2.py
+++ Lesson_8/task_2.py
@@ -1,11 +1,27 @@
 import random
 
-#n = int(input('Введіть число: '))
-n = 5
-Matrix = [[random.randint(0,100) for x in range(n)] for y in range(n)]
+n = int(input('Введіть число: '))
+
+Matrix = [[random.randint(0, 10) for x in range(n)] for y in range(n)]
+
+s_d = 0  # сума по діагоналі
+s_k = 0  # cума останнього стовбця
 
 for x in range(n):
-    print(Matrix[x])
+    print(Matrix[x])  # вивід матриці
 
-    s = sum(Matrix[3])
+for j in range(n):
+    s_d += Matrix[j][j]
+    s_k += Matrix[j][n-1]
+
+print("*"*60)
+
+print("cума чисел по діагоналі (зліва): " + str(s_d))
+print("cума чисел станнього стовбця: " + str(s_k))
+
+#  2 варіант коду щодо суми чисел матриці з "sum" та "zip"
+s = [sum(i) for i in zip(*Matrix)]
+print("cума чисел останнього стовбця (2 варіант коду): " + str(s[n-1]))
+
+print("*"*60)
 
--- My_draft/draft__2.py
+++ My_draft/draft__2.py
@@ -1,18 +1,22 @@
 import random
 
-#n = int(input('Введіть число: '))
-n = 5
+n = int(input('Введіть число: '))
+
 Matrix = [[random.randint(0, 10) for x in range(n)] for y in range(n)]
 
+s_d = 0  # сума по діагоналі
+s_k = 0  # cума останнього стовбця
+
 for x in range(n):
-    print(Matrix[x])
+    print(Matrix[x])  # вивід матриці
 
+for j in range(n):
+    s_d += Matrix[j][j]
+    s_k += Matrix[j][n-1]
 
+print("cума чисел по діагоналі (зліва): " + str(s_d))
+print("cума чисел станнього стовбця: " + str(s_k))
+
+#  2 варіант коду щодо суми чисел матриці з "sum" та "zip"
 s = [sum(i) for i in zip(*Matrix)]
-print(s[n-1])
-print(s)
-
-
-s_2 = [sum(1) for i in zip(*Matrix)]
-print(s_6[n-1])
-print(s)
+print("cума чисел останнього стовбця (2 варіант коду): " + str(s[n-1]))
