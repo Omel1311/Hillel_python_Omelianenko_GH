1911ab6c820414b198e0eed33e5bf3f024dd40a0
Omel1311 <omel1311@gmail.com>
2022-08-18 18:46:43 +0300

    random 20

--- Lesson_9/task_2.py
+++ Lesson_9/task_2.py
@@ -1,4 +1,17 @@
 import random
 
+random_list_1 = [x for x in range(1,21)]
+#  print(list(random_list_1)) перевірка 1
 
-for
+random_list_2 = [random.randint(1, 100) for x in range(20)]
+#  print(list(random_list_2)) перевірка 2
+
+dict_1 = {k: v for k, v in zip(random_list_1, random_list_2)}
+print(type(dict_1))
+print(dict_1)
+
+m = 1
+for i in dict_1.values():
+    m = i*m
+
+print(f"Результат перемноження значень в словнику: {m}")
