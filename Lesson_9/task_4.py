c039f96476e9dde0d1da33761b61b63e99106f8f
Omel1311 <omel1311@gmail.com>
2022-08-18 22:09:46 +0300

    створення словника із списків

---
+++ Lesson_9/task_4.py
@@ -0,0 +1,8 @@
+list_1 = ['w', 'e', 'r', 't', 'y', 'h', 'd', 'h']
+list_2 = [1, 2, 3, 4, 5, 6, 7, 8]
+
+dict_1 = {k: v for k, v in zip(list_1, list_2)}
+
+# print(type(dict_1))  перевірка відповідності типу обєкта
+
+print(dict_1)
