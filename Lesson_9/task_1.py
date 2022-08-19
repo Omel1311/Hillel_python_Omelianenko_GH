45038a4ad49c998bf98badceb4bf94594830c7d4
Omel1311 <omel1311@gmail.com>
2022-08-19 10:02:46 +0300

    обєднання словників 2

--- Lesson_9/task_1.py
+++ Lesson_9/task_1.py
@@ -1,10 +1,10 @@
 dictionary_1 = {'a': 300, 'b': 400}
 dictionary_2 = {'c': 500, 'd': 600}
 
-dictionary_1.update(dictionary_2)  # cgj
+dictionary_1.update(dictionary_2)  # спосіб 1
 
 print(f'спосіб 1: {dictionary_1}')
 
-dictionary_3 = {**dictionary_1, **dictionary_2}
+dictionary_3 = {**dictionary_1, **dictionary_2}  # спосіб 2
 
 print(f'спосіб 2 (зі створенням нового словника dictionary_3) : {dictionary_3}')
