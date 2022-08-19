209ab76ae0ef0423af3641599e2484bb4f74f055
Omel1311 <omel1311@gmail.com>
2022-08-19 09:59:10 +0300

    вірш

--- Lesson_9/task_6.py
+++ Lesson_9/task_6.py
@@ -17,31 +17,38 @@ txt = '''
 replace_1 = txt.replace(",", "")
 replace_2 = replace_1.replace(".", "")
 replace_3 = replace_2.replace("—", "")
+replace_4 = replace_3.lower()
 
-str_split = replace_3.split()
+str_split = replace_4.split()  # тут відображається текст з малої літери та без розділових знаків
+
 
 dict_1 = {s: str_split.count(s) for s in str_split}
 
 lst = dict_1.items()
 lst_1 = list(lst)
+print("*"*160)
+print(lst_1)  # повний список з ключами і значеннями
 
 keys = list(dict_1.keys())
 values = list(dict_1.values())
+#  print(values) перевірка values
 max_values = 1
 max_keys = 0
+max_values_2 = 1
+max_keys_2 = 0
 
-for i in range(len(values) - 1):
+for i in range(len(values)):
     if values[i] > max_values:
         max_values = values[i]
         max_keys = keys[i]
 
-a = {k:v for k,v in lst if v == 1}
+a = {k: v for k, v in lst if v == 1}
 min_items = a.items()
 k = list(min_items)
 
 print("*"*160)
 print(f'Найбільша кількість слів: {max_keys, max_values}')
 print("*"*160)
-print((f'Найменша кількість слів: {k}'))
+print(f'Найменша кількість слів: {k}')
 print("*"*160)
 
--- My_draft/draft_1.py
+++ My_draft/draft_1.py
@@ -1,55 +1,11 @@
-# q = [1, 3, 4, 5, 666]
-# e = ['222', "sdfasdfafsa", 'asdasdasd']
-#
-# d = dict(zip(q, e))
-# print(d)
-#
-# ae = dict(qwqw=22, asdasd=44, p=45, a=8989868968)
-# ae['p'] = 44
-#
-# print(list(ae.values()))
-# print(list(ae.keys()))
-#
-# print(list(ae.items()))
-# print(ae.get(66, 34564))
-# print(ae)
-# del (ae['p'])
-# print(ae)
-# ae['qweqw'] = 666
-# print(ae)
-# ae['p'] = 'dqadqd'
-# print(ae)
-# ae['p'] = ['asssss', 12313, 12312313]
-# print(ae)
-# print('fwf' in ae)
-# print(ae)
-# print(len(ae))
-#
-#
-# z = {'i':2, 'l':0000, 'x':[3,5,5,]}
-# z['x'][2] = 9
-# print(z)
-# o = dict.fromkeys([1,4,5,6],5)
-# # print(o)
-#
-e = {'q':3,'we':4,'w':'sad'}
-# a= e.count(3)
-# print(a)
-# a = 'asdasd adsasd hhh htry jjk k trt ertyee wjm, klt66 553 323'
-# q = a.split()
-# print(q)
-# s = q.count('')
-# len_f = len(s)
-# # print(s)
-# print(len_f)
-# print(e)
-# s = "22"
 
-e.pop('q')
-print(e)
-v = {1:2,3:7, 'w':3}
-print(v)
+def sum (x,y):
+    '''hoi ho ihoh
 
-nn= e.keys() ^ v.keys()
-print(nn)
 
+
+    ]'
+
+    [l[joi'''
+    return (x+y)
+print()
