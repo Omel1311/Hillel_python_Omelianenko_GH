6a81fc81e04e4d8076fb61a42add16be8f22bfb8
Omel1311 <omel1311@gmail.com>
2022-08-23 22:35:33 +0300

    функція сума двох чисел (2 функції, 4 теста)

---
+++ Lesson_11/task_1.py
@@ -0,0 +1,20 @@
+def test_1(n, *f):
+    for i in f:
+        for j in f:
+            if i + j == n:
+                print(f'Тест 1: сума {i, j} = {n}')
+                return n, f
+
+
+def test_2(v, *t):
+    for q in t:
+        for w in t:
+            if q + w == v:
+                print(f'Тест 2: сума {q, w} = {v}')
+                return v, t
+
+
+test_1(3, 2, 4, 1)
+test_1(100, 40, 50, 5, 30, 30)
+test_2(3, 3, 1, 2, 2)
+test_2(6,3,3,3,3,3,3)
