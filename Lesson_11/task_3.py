2fd74342e20e90ac518ef31034dda883acb00e95
Omel1311 <omel1311@gmail.com>
2022-08-30 18:24:52 +0300

    функція-генератор доопрацьована

--- Lesson_11/task_3.py
+++ Lesson_11/task_3.py
@@ -1,17 +1,13 @@
-def prime_numbers(n_1, n_2):
-    lst_prime_numbers = []
-    for j in range(n_1, n_2):
-        prime_number = True
-        for i in range(2, j):
-            if j % i == 0:
-                prime_number = False
-        if prime_number:
-            lst_prime_numbers.append(j)
-
-    print(lst_prime_numbers)
-    print('*'*160)
+def prime(x, y):
+    while x < y+1:
+        if x > 1:
+            for i in range(2, x):
+                if (x % i) == 0:
+                    break
+            else:
+                yield x
+        x += 1
 
 
-prime_numbers(1, 1000)
-
-prime_numbers(30, 50)
+for e in prime(1, 100):
+    print(e, end=" ")
