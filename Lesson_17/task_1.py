c92d708579254ac0a66bab5e7f3450e4f5526205
Omel1311 <omel1311@gmail.com>
2022-09-10 18:25:08 +0300

    текстовий файл запис користувачем

---
+++ Lesson_17/task_1.py
@@ -0,0 +1,8 @@
+fid = open('text_file.txt', 'w')
+
+while True:
+    text = input("Введіть дані: ")
+    if text == ' ':
+        print('Введення даних заврешено!')
+        break
+    fid.write(text+'\n')
