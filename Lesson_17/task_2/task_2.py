d20f30b81eaefdb9acd7f67dc1f3fb29ebce60fa
Omel1311 <omel1311@gmail.com>
2022-09-11 17:54:47 +0300

    longest_words

---
+++ Lesson_17/task_2/task_2.py
@@ -0,0 +1,29 @@
+def longest_words(file):
+    """
+    longest_words - функція, яка виводить слово, що має максимальну довжину (або список слів, якщо таких кілька)
+
+    :param file:
+    :return:
+
+    """
+    text = open(file, 'r', encoding='utf-8')
+
+    text_split = text.read().split()  # читаємо та форматуємо текст
+
+    dict = {i: len(i) for i in text_split}  # створюємо словник
+
+    #  print(dict) тестова пеервірка довжини всіх слів
+    max_val = max(dict.values())
+
+    final_dict = {k: v for k, v in dict.items() if v == max_val}  # визначаємо в словнику пару з макс.значенням
+
+    print(f'Слова з максимальною довжиною: {final_dict}')
+
+    text.close()
+    return final_dict
+
+
+
+if __name__ == "__main__":
+    file = 'article.txt'
+    longest_words(file)
